3f5a7c7cbafb951b1ceedd25938e273890141a9a
Zach Brandt <zach.brandt@openphone.co>
2023-11-02 13:33:08 -0400

    update shards to be a fraction

--- .github/actions/setup-matrix/setup-matrix.ps1
+++ .github/actions/setup-matrix/setup-matrix.ps1
@@ -2,7 +2,7 @@ $Jobs = @()
 
 for (($i = 1); $i -le $Env:shards; $i++)
 {
-    $Jobs += $i
+    $Jobs += ""+$i+"/"+$Env:shards
 }
 
 Write-Host "::set-output name=shards::$( $Jobs | ConvertTo-JSON -Compress ))"
